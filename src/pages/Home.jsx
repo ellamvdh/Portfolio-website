b034c220109ebd55256cd2343eddc7bdfd275464
Ella Marije <fungirl2050@gmail.com>
2026-01-06 13:40:41 +0100

    fixed a little

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,18 +46,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="projects-section">
-        <div className="projects-container">
-          <h2 className="projects-title">Mijn Projecten</h2>
-
-          {/* Grid with ProjectCards */}
-          <div className="projects-grid">
-            {projects.map((project) => (
-              <ProjectCard key={project.id} project={project} />
-            ))}
-          </div>
-        </div>
-      </section>
+      
 
     </div>
   );
